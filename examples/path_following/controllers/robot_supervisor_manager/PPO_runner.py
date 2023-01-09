6e594de8bda2b6b609dea0dc29aab7edc1f7518a
tsampazk <27914645+tsampazk@users.noreply.github.com>
2023-01-09 17:01:20 +0200

    Modified difficulty

--- examples/path_following/controllers/robot_supervisor_manager/PPO_runner.py
+++ examples/path_following/controllers/robot_supervisor_manager/PPO_runner.py
@@ -19,10 +19,10 @@ def run():
     solved = False  # Whether the solved requirement is met
     avg_episode_action_probs = []  # Save average episode taken actions probability to plot later
     difficulty = {
-        0: {"number_of_obstacles": 0, "min_target_dist": 1, "max_target_dist": 1},
-        500: {"number_of_obstacles": 4, "min_target_dist": 1, "max_target_dist": 2},
-        1500: {"number_of_obstacles": 8, "min_target_dist": 1, "max_target_dist": 3},
-        3000: {"number_of_obstacles": 12, "min_target_dist": 1, "max_target_dist": 4},
+        0: {"number_of_obstacles": 8, "min_target_dist": 2, "max_target_dist": 6},
+        250: {"number_of_obstacles": 4, "min_target_dist": 1, "max_target_dist": 2},
+        1000: {"number_of_obstacles": 8, "min_target_dist": 1, "max_target_dist": 3},
+        2500: {"number_of_obstacles": 12, "min_target_dist": 1, "max_target_dist": 4},
         5000: {"number_of_obstacles": 16, "min_target_dist": 1, "max_target_dist": 5},
         10000: {"number_of_obstacles": 20, "min_target_dist": 1, "max_target_dist": 6},
         12000: {"number_of_obstacles": 20, "min_target_dist": 2, "max_target_dist": 6},
