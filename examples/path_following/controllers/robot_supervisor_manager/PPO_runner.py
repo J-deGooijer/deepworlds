cbc0769048068cd96959b95ad0f8013ce6a8f07f
tsampazk <27914645+tsampazk@users.noreply.github.com>
2023-01-27 14:16:41 +0200

    Major cleanup/refactoring and save training metrics per checkpoint

--- examples/path_following/controllers/robot_supervisor_manager/PPO_runner.py
+++ examples/path_following/controllers/robot_supervisor_manager/PPO_runner.py
@@ -1,8 +1,49 @@
-from numpy import convolve, ones, mean
 import os
+import pickle
+from numpy import convolve, ones, mean
 from path_following_robot_supervisor import PathFollowingRobotSupervisor
 from agent.PPO_agent import PPOAgent, Transition
 from utilities import plot_data
+
+
+def is_solved(reward_list):
+    """
+    This method checks whether the task is solved, so training terminates.
+    Solved condition requires that the average episode score of last 10 episodes is over half the
+    theoretical maximum of an episode's reward. Empirical observations show that when this average
+    reward per episode is achieved, the agent is well-trained.
+
+    The maximum value is found empirically in various map sizes and is calculated dynamically with
+    a linear regression fit based on the current map size assuming the map is square.
+
+    This maximum is infeasible for the agent to achieve as it requires the agent to have a perfect policy
+    and that a straight unobstructed path to the target exists from the starting position.
+    Thus, it is divided by 2 which in practice proved to be achievable.
+
+    :return: True if task is solved, False otherwise
+    :rtype: bool
+    """
+    # TODO redo this
+    # avg_score_limit = (1317.196 * self.map.size()[0] + 4820.286) * 10
+    #
+    # if len(reward_list) >= 10:  # Over 10 episodes thus far
+    #     if np.mean(reward_list[-10:]) > avg_score_limit:  # Last 10 episode scores average value
+    #         return True
+
+
+def print_episode_info(env, experiment_name, episode_count, average_episode_action_prob, episode_metrics):
+    print(f"{'#':#<50}")
+    print(f"Experiment \"{experiment_name}\" - episode #{episode_count + 1}\n ")
+    print("Reward")
+    print(f"{'Total':<12}: {episode_metrics['reward']:.2f}")
+    for key in episode_metrics["reward_sums"].keys():
+        print(f"{key[0].upper() + key[1:]:<12}: {episode_metrics['reward_sums'][key]:.2f}")
+    print(" ")
+    print(f"All actions average probability : {average_episode_action_prob * 100:.2f} %")
+    actions = ["forward", "left", "right", "stop", "backward"]
+    for i in range(env.action_space.n):
+        action = actions[i]
+        print(f"Average probability for {action:<8}: {mean(episode_metrics['action_probs'][str(i)]) * 100:.2f} %")
 
 
 def run():
@@ -12,29 +53,16 @@ def run():
     No desc
     """
 
-    # Initialize supervisor object
-    env = PathFollowingRobotSupervisor(experiment_description)
-
-    # The agent used here is trained with the PPO algorithm (https://arxiv.org/abs/1707.06347).
-    # We pass the number of inputs and the number of outputs, taken from the gym spaces
-    agent = PPOAgent(env.observation_space.shape[0], env.action_space.n)
-
-    if parent_dir is not None:
-        if not os.path.exists(parent_dir):
-            os.mkdir(parent_dir)
-
-        parent_dir = os.path.join(parent_dir, experiment_name)
-        if not os.path.exists(parent_dir):
-            os.mkdir(parent_dir)
-
+    cuda = False
     episode_count = 0
     episode_limit = 10000
     episodes_per_checkpoint = 250
     solved = False  # Whether the solved requirement is met
-    all_episodes_action_probs = []
-    all_episodes_avg_action_probs = []
-    all_episodes_final_distance = []
-    all_episodes_rewards = []
+    training_metrics = {"total_rewards": [], "rewards_breakdown": [],
+                        "action_probs": [], "avg_action_probs": [],
+                        "final_distances": []
+                        }
+
     difficulty = {
         0: {"number_of_obstacles": 25, "min_target_dist": 1, "max_target_dist": 1},
         250: {"number_of_obstacles": 25, "min_target_dist": 1, "max_target_dist": 2},
@@ -58,105 +86,130 @@ def run():
         4750: {"number_of_obstacles": 25, "min_target_dist": 10, "max_target_dist": 11},
         5000: {"number_of_obstacles": 25, "min_target_dist": 10, "max_target_dist": 12},
     }
+
+    # Initialize supervisor object
+    env = PathFollowingRobotSupervisor(experiment_description)
+    # The agent used here is trained with the PPO algorithm (https://arxiv.org/abs/1707.06347).
+    # We pass the number of inputs and the number of outputs, taken from the gym spaces
+    agent = PPOAgent(env.observation_space.shape[0], env.action_space.n, use_cuda=cuda)
+
+    # Initialize directories and save experiment parameters
     if parent_dir is not None:
+        if not os.path.exists(parent_dir):
+            os.mkdir(parent_dir)
+        parent_dir = os.path.join(parent_dir, experiment_name)
+        if not os.path.exists(parent_dir):
+            os.mkdir(parent_dir)
         # Save experiment setup to json file
         env.export_parameters(os.path.join(parent_dir, "experiment_parameters.json"), agent, difficulty, episode_limit)
 
     # Run outer loop until the episodes limit is reached or the task is solved
     while not solved and episode_count < episode_limit:
+
+        # Set difficulty
         if episode_count in difficulty.keys():
             env.set_difficulty(difficulty[episode_count])
 
-        state = env.reset()  # Reset robot and get starting observation
-        env.episode_score = 0
-        # This dict holds the probabilities of each chosen action
-        episode_action_probs = {str(i): [] for i in range(env.action_space.n)}
-        episode_final_distance = 1.0  # This is the final distance reached in the episode normalized in [0.0, 1.0]
-        episode_reward_sums = {"target": 0, "sensors": 0, "path": 0, "reach_target": 0, "collision": 0}
+        # Reset robot and get starting observation
+        state = env.reset()
 
-        # Inner loop is the episode loop
+        # Initialize episode_metrics dictionary to save all the metrics
+        episode_metrics = {"reward": 0.0,
+                           "reward_sums": {"target": 0, "sensors": 0, "path": 0, "reach_target": 0, "collision": 0},
+                           "final_distance": 1.0,
+                           "action_probs": {str(i): [] for i in range(env.action_space.n)}
+                           }
+
+        # Episode loop
         for step in range(env.steps_per_episode):
             # In training mode the agent samples from the probability distribution, naturally implementing exploration
             selected_action, action_prob = agent.work(state, type_="selectAction")
             # Save the current selected_action's probability
-            episode_action_probs[str(selected_action)].append(action_prob)
+            episode_metrics["action_probs"][str(selected_action)].append(action_prob)
 
-            # Step the supervisor to get the current selected_action reward, the new state and whether we reached the
-            # done condition
-            new_state, reward, done, info = env.step(selected_action)
+            # Step the supervisor to advance the simulation and get the current selected_action reward,
+            # the new state and whether we reached the done condition
+            new_state, reward, done, _ = env.step(selected_action)
 
             # Save the current state transition in agent's memory
-            trans = Transition(state, selected_action, action_prob, reward["total"], new_state)
-            agent.store_transition(trans)
+            agent.store_transition(Transition(state, selected_action, action_prob, reward["total"], new_state))
 
-            env.episode_score += reward["total"]  # Accumulate episode reward
-            for key in episode_reward_sums.keys():
-                episode_reward_sums[key] += reward[key]  # Accumulate various rewards
+            episode_metrics["reward"] += reward["total"]  # Accumulate episode reward
+            for key in episode_metrics["reward_sums"].keys():
+                episode_metrics["reward_sums"][key] += reward[key]  # Accumulate various rewards
             if done or step == env.steps_per_episode - 1:
                 # Save final distance achieved from final state
-                episode_final_distance = state[0]
-
+                episode_metrics["final_distance"] = state[0]
                 agent.train_step(batch_size=step + 1)
-                solved = env.solved()  # Check whether the task is solved
-
-                # Save agent
-                if episode_count % episodes_per_checkpoint == 0 \
-                        and episode_count != 0 \
-                        or episode_count == episode_limit - 1:
-                    if parent_dir is not None:
-                        checkpoint_dir = os.path.join(parent_dir, "checkpoints")
-                        if not os.path.exists(checkpoint_dir):
-                            os.mkdir(checkpoint_dir)
-                        agent.save(os.path.join(checkpoint_dir, str(episode_count)))
                 break
 
             state = new_state  # state for next step is current step's new_state
-        # End of the episode, print and save some stats
-        print(f"{'#':#<50}")
-        print(f"Experiment \"{experiment_name}\" - episode #{episode_count + 1}\n ")
-        print("Reward")
-        print(f"{'Total':<12}: {env.episode_score:.2f}")
-        for key in episode_reward_sums.keys():
-            print(f"{key[0].upper() + key[1:]:<12}: {episode_reward_sums[key]:.2f}")
-        print(" ")
+
+        # Check whether the task is solved
+        solved = is_solved([])
+
+        # Save agent and metrics
+        if episode_count % episodes_per_checkpoint == 0 \
+                and episode_count != 0 \
+                or episode_count == episode_limit - 1:
+            if parent_dir is not None:
+                checkpoint_dir = os.path.join(parent_dir, "checkpoints")
+                if not os.path.exists(checkpoint_dir):
+                    os.mkdir(checkpoint_dir)
+                agent.save(os.path.join(checkpoint_dir, str(episode_count)))
+
+                result_dict = {"episodes_reward": training_metrics["total_rewards"],
+                               "episodes_reward_breakdown": training_metrics["rewards_breakdown"],
+                               "episodes_action_probs": training_metrics["action_probs"],
+                               "episodes_avg_action_prob": training_metrics["avg_action_probs"],
+                               "episodes_final_distance": training_metrics["final_distances"]
+                               }
+                with open(os.path.join(parent_dir, experiment_name + "_results.pkl"), "wb") as f:
+                    pickle.dump(result_dict, f)
+
+        # End of the episode save metrics
         # Save the episode's score
-        env.episode_score_list.append(env.episode_score)
-        all_episodes_rewards.append(episode_reward_sums)
+        training_metrics["total_rewards"].append(episode_metrics["reward"])
+        training_metrics["rewards_breakdown"].append(episode_metrics["reward_sums"])
+
         # Save the episode's action probabilities
-        all_episodes_action_probs.append(episode_action_probs)
+        training_metrics["action_probs"].append(episode_metrics["action_probs"])
+
+        # Calculate and save the average action probability
         all_probs = []
         for i in range(env.action_space.n):
-            all_probs.extend(episode_action_probs[str(i)])
-        # Save the average action probability
+            all_probs.extend(episode_metrics["action_probs"][str(i)])
         average_episode_action_prob = mean(all_probs)
-        all_episodes_avg_action_probs.append(average_episode_action_prob)
-        print(f"All actions average probability : {average_episode_action_prob * 100:.2f} %")
-        actions = ["forward", "left", "right", "stop", "backward"]
-        for i in range(env.action_space.n):
-            action = actions[i]
-            print(f"Average probability for {action:<8}: {mean(episode_action_probs[str(i)]) * 100:.2f} %")
+        training_metrics["avg_action_probs"].append(average_episode_action_prob)
+
         # Save the episode final distance achieved
-        all_episodes_final_distance.append(episode_final_distance)
+        training_metrics["final_distances"].append(episode_metrics["final_distance"])
+
+        # Print information
+        print_episode_info(env, experiment_name, episode_count, average_episode_action_prob, episode_metrics)
 
         episode_count += 1  # Increment episode counter
 
+    # End of training
     try:
+        # Plot the main convergence metrics of reward per episode and average action probability per episode
         # np.convolve is used as a moving average, see https://stackoverflow.com/a/22621523
         moving_avg_n = 10
-        plot_data(convolve(env.episode_score_list, ones((moving_avg_n,)) / moving_avg_n, mode='valid'),  # NOQA
+        plot_data(convolve(training_metrics["total_rewards"], ones((moving_avg_n,)) / moving_avg_n, mode='valid'),  # NOQA
                   "episode", "episode score", "Episode scores over episodes - " + experiment_name)
+        plot_data(convolve(training_metrics["avg_action_probs"], ones((moving_avg_n,)) / moving_avg_n, mode='valid'),  # NOQA
+                  "episode", "average action probability", "Average action probability over episodes - " + experiment_name)
     except Exception as e:
         print("Plotting failed:", e)
 
-    import pickle
-    result_dict = {"episodes_reward": env.episode_score_list,
-                   "episodes_reward_breakdown": all_episodes_rewards,
-                   "episodes_avg_action_prob": all_episodes_avg_action_probs,
-                   "episodes_action_probs": all_episodes_action_probs,
-                   "episodes_final_distance": all_episodes_final_distance
+    result_dict = {"episodes_reward": training_metrics["total_rewards"],
+                   "episodes_reward_breakdown": training_metrics["rewards_breakdown"],
+                   "episodes_action_probs": training_metrics["action_probs"],
+                   "episodes_avg_action_prob": training_metrics["avg_action_probs"],
+                   "episodes_final_distance": training_metrics["final_distances"]
                    }
     if parent_dir is not None:
-        with open(os.path.join(parent_dir, experiment_name + "_results.pkl"), 'wb') as f:
+        with open(os.path.join(parent_dir, experiment_name + "_results.pkl"), "wb") as f:
             pickle.dump(result_dict, f)
 
     if not solved:
@@ -165,36 +218,38 @@ def run():
         print("Task is solved, deploying agent for testing...")
 
     state = env.reset()
-    env.episode_score = 0
     while True:
-        episode_action_probs = {str(i): [] for i in range(env.action_space.n)}
-        episode_reward_sums = {"target": 0, "sensors": 0, "path": 0, "reach_target": 0, "collision": 0}
+        episode_metrics = {"reward": 0.0,
+                           "reward_sums": {"target": 0, "sensors": 0, "path": 0, "reach_target": 0, "collision": 0},
+                           "final_distance": 1.0,
+                           "action_probs": {str(i): [] for i in range(env.action_space.n)}
+                           }
         for step in range(env.steps_per_episode):
             selected_action, action_prob = agent.work(state, type_="selectActionMax")
             state, reward, done, _ = env.step(selected_action)
 
-            episode_action_probs[str(selected_action)].append(action_prob)
+            episode_metrics["action_probs"][str(selected_action)].append(action_prob)
 
-            env.episode_score += reward["total"]  # Accumulate episode reward
-            for key in episode_reward_sums.keys():
-                episode_reward_sums[key] += reward[key]  # Accumulate various rewards
+            episode_metrics["reward"] += reward["total"]  # Accumulate episode reward
+            for key in episode_metrics["reward_sums"].keys():
+                episode_metrics["reward_sums"][key] += reward[key]  # Accumulate various rewards
 
             if done or step == env.steps_per_episode - 1:
                 print(f"{'#':#<50}")
                 print(f"Experiment \"{experiment_name}\"\n ")
                 print("Reward")
-                print(f"{'Total':<12}: {env.episode_score:.2f}")
-                for key in episode_reward_sums.keys():
-                    print(f"{key[0].upper() + key[1:]:<12}: {episode_reward_sums[key]:.2f}")
+                print(f"{'Total':<12}: {episode_metrics['reward']:.2f}")
+                for key in episode_metrics["reward_sums"].keys():
+                    print(f"{key[0].upper() + key[1:]:<12}: {episode_metrics['reward_sums'][key]:.2f}")
                 print(" ")
                 all_probs = []
                 for i in range(env.action_space.n):
-                    all_probs.extend(episode_action_probs[str(i)])
+                    all_probs.extend(episode_metrics["action_probs"][str(i)])
                 average_episode_action_prob = mean(all_probs)
                 print(f"All actions average probability : {average_episode_action_prob * 100:.2f} %")
                 actions = ["forward", "left", "right", "stop", "backward"]
                 for i in range(env.action_space.n):
                     action = actions[i]
-                    print(f"Average probability for {action:<8}: {mean(episode_action_probs[str(i)]) * 100:.2f} %")
-                env.episode_score = 0
+                    print(f"Average probability for {action:<8}: {mean(episode_metrics['action_probs'][str(i)]) * 100:.2f} %")
+                episode_metrics["reward"] = 0
                 state = env.reset()
