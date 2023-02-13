6e1bed75e4bfad801fb06edffb1564e94ba5e2e0
tsampazk <27914645+tsampazk@users.noreply.github.com>
2023-02-13 09:56:21 +0200

    Added more hyperparams, added export of setup, cleanup

--- examples/path_following/controllers/robot_supervisor_manager/PPO_runner.py
+++ examples/path_following/controllers/robot_supervisor_manager/PPO_runner.py
@@ -3,6 +3,7 @@ from sb3_contrib import MaskablePPO
 from sb3_contrib.common.wrappers import ActionMasker
 from gym.wrappers import TimeLimit
 import torch
+import os
 
 
 def mask_fn(env):
@@ -11,13 +12,15 @@ def mask_fn(env):
 
 def run():
     # Environment setup
-    total_timesteps = 1_000_000
+    total_timesteps = 2_500_000
     n_steps = 5120  # Number of steps between training, effectively the size of the buffer to train on
     batch_size = 128
     maximum_episode_steps = 10240
     gamma = 0.995
-    target_kl = 0.04
-    experiment_name = "basic_baseline"
+    target_kl = None
+    vf_coef = 0.5
+    ent_coef = 0.01
+    experiment_name = "baseline"
     experiment_description = """Window 5-10, sb3"""
     reset_on_collisions = 500
     verbose = False
@@ -31,9 +34,16 @@ def run():
     tar_reach_weight = 10.0
     col_weight = 2.0
     time_penalty_weight = 0.1
+    net_arch = dict(pi=[512, 256, 128], vf=[1024, 512, 256])
     # Map setup
     map_w, map_h = 7, 7
     cell_size = None
+    difficulty_dict = {"diff_1": {"type": "box", "number_of_obstacles": 4, "min_target_dist": 2, "max_target_dist": 3},
+                       "diff_2": {"type": "box", "number_of_obstacles": 6, "min_target_dist": 3, "max_target_dist": 4},
+                       "diff_3": {"type": "box", "number_of_obstacles": 8, "min_target_dist": 4, "max_target_dist": 5},
+                       "diff_4": {"type": "box", "number_of_obstacles": 10, "min_target_dist": 5, "max_target_dist": 6},
+                       }
+
     env = TimeLimit(PathFollowingRobotSupervisor(experiment_description, window_latest_dense=window_latest_dense,
                                                  window_older_diluted=window_older_diluted,
                                                  reset_on_collisions=reset_on_collisions, manual_control=manual_control,
@@ -45,21 +55,32 @@ def run():
                                                  map_width=map_w, map_height=map_h, cell_size=cell_size),
                     maximum_episode_steps)
     env = ActionMasker(env, action_mask_fn=mask_fn)  # NOQA
-    policy_kwargs = dict(activation_fn=torch.nn.ReLU,
-                         net_arch=dict(pi=[256, 128, 64], vf=[512, 256, 128]))
+
+    experiment_dir = f"./experiments/{experiment_name}"
+    if not os.path.exists(experiment_dir):
+        os.makedirs(experiment_dir)
+    env.export_parameters(experiment_dir + f"/{experiment_name}.json",
+                          net_arch, gamma, target_kl, vf_coef, ent_coef,
+                          difficulty_dict, maximum_episode_steps)
+
+    policy_kwargs = dict(activation_fn=torch.nn.ReLU, net_arch=net_arch)
     model = MaskablePPO("MlpPolicy", env, policy_kwargs=policy_kwargs,
-                        n_steps=n_steps, batch_size=batch_size, gamma=gamma, target_kl=target_kl, vf_coef=0.5, ent_coef=0.01,
-                        verbose=1, tensorboard_log="./experiments/" + experiment_name)
-    env.set_difficulty({"type": "box", "number_of_obstacles": 4, "min_target_dist": 2, "max_target_dist": 3})
+                        n_steps=n_steps, batch_size=batch_size, gamma=gamma,
+                        target_kl=target_kl, vf_coef=vf_coef, ent_coef=ent_coef,
+                        verbose=1, tensorboard_log=f"./experiments/{experiment_name}")
+    env.set_difficulty(difficulty_dict["diff_1"])
     model.learn(total_timesteps=total_timesteps, tb_log_name="difficulty_1")
-    env.set_difficulty({"type": "box", "number_of_obstacles": 6, "min_target_dist": 3, "max_target_dist": 4})
+    model.save(f"./experiments/{experiment_name}/experiment_name_diff_1_agent")
+    env.set_difficulty(difficulty_dict["diff_2"])
     model.learn(total_timesteps=total_timesteps, tb_log_name="difficulty_2")
-    env.set_difficulty({"type": "box", "number_of_obstacles": 8, "min_target_dist": 4, "max_target_dist": 5})
+    model.save(f"./experiments/{experiment_name}/experiment_name_diff_2_agent")
+    env.set_difficulty(difficulty_dict["diff_3"])
     model.learn(total_timesteps=total_timesteps, tb_log_name="difficulty_3")
-    env.set_difficulty({"type": "box", "number_of_obstacles": 10, "min_target_dist": 5, "max_target_dist": 6})
+    model.save(f"./experiments/{experiment_name}/experiment_name_diff_3_agent")
+    env.set_difficulty(difficulty_dict["diff_4"])
     model.learn(total_timesteps=total_timesteps, tb_log_name="difficulty_4")
-    model.save("mlp_test_sparse")
-    # model = PPO.load("mlp_test_sparse")
+    model.save(f"./experiments/{experiment_name}/experiment_name_diff_4_agent")
+    # model = PPO.load(f"./experiments/{experiment_name}/experiment_name_diff_4_agent")
 
     obs = env.reset()
     while True:
