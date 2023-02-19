1ccbcbfc4eb576755d3fab39ff5448ce737c0ec0
tsampazk <27914645+tsampazk@users.noreply.github.com>
2023-02-19 19:06:26 +0200

    improved testing

--- examples/path_following/controllers/robot_supervisor_manager/PPO_runner.py
+++ examples/path_following/controllers/robot_supervisor_manager/PPO_runner.py
@@ -109,8 +109,10 @@ def run():
     vf_coef = 0.5
     ent_coef = 0.01
     experiment_name = "baseline"
+    experiment_dir = f"./experiments/{experiment_name}"
     experiment_description = """Baseline description."""
     reset_on_collisions = 500
+    deterministic = False  # Whether action is deterministic when testing
     manual_control = False
     load_path = None
     # load_path = "./experiments/" + experiment_name + f"/{experiment_name}_diff_4_agent.zip"
@@ -156,7 +158,6 @@ def run():
                     maximum_episode_steps)
     env = ActionMasker(env, action_mask_fn=mask_fn)  # NOQA
 
-    experiment_dir = f"./experiments/{experiment_name}"
     if load_path is None:
         if not os.path.exists(experiment_dir):
             os.makedirs(experiment_dir)
@@ -196,45 +197,52 @@ def run():
 
     obs = env.reset()
     cumulative_rew = 0.0
-    count = 0
+    tests_count = 0
     tests_per_difficulty = 3
-    deterministic = False
-    print(f"Experiment name: {experiment_name}")
+    print(f"Experiment name: {experiment_name}, deterministic: {deterministic}")
     import csv
     header = [experiment_name]
     for i in range(len(test_difficulty)):
         for j in range(tests_per_difficulty):
             header.append(f"{test_difficulty[i]}")
 
-    episode_rewards = [""]
-    done_reasons = [""]
-    with open(experiment_dir + "/testing_results.csv", 'w', encoding='UTF8') as f:
+    episode_rewards = ["reward"]
+    done_reasons = ["done_reason"]
+    steps_row = ["steps"]
+    file_name = "/testing_results.csv" if not deterministic else "/testing_results_det.csv"
+    with open(experiment_dir + file_name, 'w', encoding='UTF8') as f:
         writer = csv.writer(f)
         writer.writerow(header)
+        steps = 0
         while True:
             action_masks = mask_fn(env)
             action, _states = model.predict(obs, deterministic=deterministic, action_masks=action_masks)
             obs, rewards, done, info = env.step(action)
+            steps += 1
             cumulative_rew += rewards
             if done:
                 episode_rewards.append(cumulative_rew)
+                steps_row.append(steps)
                 try:
-                    print(f"Episode reward: {cumulative_rew}, done reason: {info['done_reason']}")
+                    print(f"Episode reward: {cumulative_rew}, steps: {steps}, done reason: {info['done_reason']}")
                     done_reasons.append(info['done_reason'])
                 except KeyError:
-                    print(f"Episode reward: {cumulative_rew}, done reason: timeout")
+                    print(f"Episode reward: {cumulative_rew}, steps: {steps}, done reason: timeout")
                     done_reasons.append("timeout")
                 cumulative_rew = 0.0
-                count += 1
-                if count == tests_per_difficulty:
+                steps = 0
+
+                tests_count += 1
+                if tests_count == tests_per_difficulty:
                     diff_ind += 1
                     try:
                         env.set_difficulty(difficulty_dict[test_difficulty[diff_ind]], key=test_difficulty[diff_ind])
                     except IndexError:
                         print("Testing complete.")
                         break
-                    count = 0
+                    tests_count = 0
                 env.reset()
 
         writer.writerow(episode_rewards)
         writer.writerow(done_reasons)
+        writer.writerow(steps_row)
