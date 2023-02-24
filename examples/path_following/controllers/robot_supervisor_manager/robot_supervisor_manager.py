2c93707e6e0a0131c3d984815b771a5af72c62d4
tsampazk <27914645+tsampazk@users.noreply.github.com>
2023-02-24 17:27:04 +0200

    Refactored to run testing sequentially after training

--- examples/path_following/controllers/robot_supervisor_manager/robot_supervisor_manager.py
+++ examples/path_following/controllers/robot_supervisor_manager/robot_supervisor_manager.py
@@ -4,6 +4,8 @@ More runners for discrete RL algorithms can be added here.
 import PPO_trainer
 import PPO_testing
 
+experiment_name = "baseline"
+deterministic = False
 
-PPO_trainer.run()
-PPO_testing.run()
+env = PPO_trainer.run(experiment_name)
+PPO_testing.run(experiment_name, env, deterministic)
