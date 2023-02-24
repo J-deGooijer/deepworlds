fb73340c42a5058d78cfed91668422a357e6bb70
tsampazk <27914645+tsampazk@users.noreply.github.com>
2023-02-24 17:25:36 +0200

    Changelog: 1. Refactored to run sequentially with trainer 2. Removed unused imports 3. Automated testing now runs 100 episodes per difficulty 4. Added printing of test progress

--- examples/path_following/controllers/robot_supervisor_manager/PPO_testing.py
+++ examples/path_following/controllers/robot_supervisor_manager/PPO_testing.py
@@ -1,16 +1,13 @@
 import numpy as np
 import torch
-from gym.wrappers import TimeLimit
-from sb3_contrib.common.wrappers import ActionMasker
 from sb3_contrib import MaskablePPO
-from path_following_robot_supervisor import PathFollowingRobotSupervisor
 
 
 def mask_fn(env):
     return env.get_action_mask()
 
 
-def run():
+def run(experiment_name, env, deterministic):
     # Difficulty setup
     difficulty_dict = {"diff_1": {"type": "corridor", "number_of_obstacles": 2,
                                   "min_target_dist": 2, "max_target_dist": 2},
@@ -23,65 +20,16 @@ def run():
                        "random_diff": {"type": "random", "number_of_obstacles": 25,
                                        "min_target_dist": 5, "max_target_dist": 12}}
     test_difficulty = list(difficulty_dict.keys())
-    deterministic = False  # Whether action is deterministic when testing
-    manual_control = False
 
-    # Environment setup
-    seed = 1
-
-    maximum_episode_steps = 32_786
-
-    experiment_name = "baseline"
-    experiment_description = """Baseline description."""
+    seed = 2
+    env.set_maximum_episode_steps(env.maximum_episode_steps * 2)
     experiment_dir = f"./experiments/{experiment_name}"
     load_path = experiment_dir + f"/{experiment_name}_diff_5_agent.zip"
-
-    step_window = 1  # Latest steps of observations
-    seconds_window = 0  # How many latest seconds of observations
-    add_action_to_obs = True
-    reset_on_collisions = 4096  # Allow at least two training steps
-    ds_type = "sonar"
-    ds_noise = 0.0
-    max_ds_range = 100.0  # in cm
-    dist_sensors_threshold = 25.0
-    on_tar_threshold = 0.1
-
-    tar_d_weight_multiplier = 1.0  # When obstacles are detected, target distance reward is multiplied by this
-    tar_a_weight_multiplier = 0.0  # When obstacles are detected, target angle reward is multiplied by this
-    tar_dis_weight = 2.0
-    tar_ang_weight = 2.0
-    ds_weight = 1.0
-    obs_turning_weight = 0.0
-    tar_reach_weight = 1000.0
-    not_reach_weight = 1000.0
-    col_weight = 5.0
-    time_penalty_weight = 0.1
-
-    # Map setup
-    map_w, map_h = 7, 7
-    cell_size = None
 
     if seed is not None:
         torch.manual_seed(seed)
         np.random.seed(seed)
 
-    env = TimeLimit(PathFollowingRobotSupervisor(experiment_description, maximum_episode_steps, step_window=step_window,
-                                                 seconds_window=seconds_window,
-                                                 add_action_to_obs=add_action_to_obs, max_ds_range=max_ds_range,
-                                                 reset_on_collisions=reset_on_collisions, manual_control=manual_control,
-                                                 on_target_threshold=on_tar_threshold,
-                                                 dist_sensors_threshold=dist_sensors_threshold, ds_type=ds_type,
-                                                 ds_noise=ds_noise,
-                                                 tar_d_weight_multiplier=tar_d_weight_multiplier,
-                                                 tar_a_weight_multiplier=tar_a_weight_multiplier,
-                                                 target_distance_weight=tar_dis_weight, tar_angle_weight=tar_ang_weight,
-                                                 dist_sensors_weight=ds_weight, obs_turning_weight=obs_turning_weight,
-                                                 tar_reach_weight=tar_reach_weight, collision_weight=col_weight,
-                                                 time_penalty_weight=time_penalty_weight,
-                                                 not_reach_weight=not_reach_weight,
-                                                 map_width=map_w, map_height=map_h, cell_size=cell_size, seed=seed),
-                    maximum_episode_steps)
-    env = ActionMasker(env, action_mask_fn=mask_fn)  # NOQA
     model = MaskablePPO.load(load_path)  # NOQA
 
     diff_ind = 0
@@ -90,7 +38,7 @@ def run():
     obs = env.reset()
     cumulative_rew = 0.0
     tests_count = 0
-    tests_per_difficulty = 3
+    tests_per_difficulty = 100
     print(f"Experiment name: {experiment_name}, deterministic: {deterministic}")
     import csv
     header = [experiment_name]
@@ -115,12 +63,13 @@ def run():
             if done:
                 episode_rewards.append(cumulative_rew)
                 steps_row.append(steps)
-                try:
-                    print(f"Episode reward: {cumulative_rew}, steps: {steps}, done reason: {info['done_reason']}")
-                    done_reasons.append(info['done_reason'])
-                except KeyError:
-                    print(f"Episode reward: {cumulative_rew}, steps: {steps}, done reason: timeout")
-                    done_reasons.append("timeout")
+                print(f"Episode reward: {cumulative_rew}, steps: {steps}, done reason: {info['done_reason']}")
+                full_test_count = (tests_per_difficulty * diff_ind) + tests_count
+                max_test_count = tests_per_difficulty * len(test_difficulty)
+                print(f"Testing progress: "
+                      f"{round((full_test_count / max_test_count) * 100.0, 2)}"
+                      f"%, {full_test_count} / {max_test_count}")
+                done_reasons.append(info['done_reason'])
                 cumulative_rew = 0.0
                 steps = 0
 
