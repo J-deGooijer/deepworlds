369f574982d32dfbeb0e6b0c3daa7960946da4cb
tsampazk <27914645+tsampazk@users.noreply.github.com>
2023-03-02 20:49:54 +0200

    Added some general easy to access parameters for starting experiments

--- examples/find_and_avoid_v2/controllers/robot_supervisor_manager/robot_supervisor_manager.py
+++ examples/find_and_avoid_v2/controllers/robot_supervisor_manager/robot_supervisor_manager.py
@@ -5,7 +5,10 @@ import PPO_trainer
 import PPO_testing
 
 experiment_name = "baseline"
+experiment_description = """Baseline description."""
 deterministic = False
+manual_control = False
+only_test = False  # If true, the trained agent from "experiment_name" will be loaded and evaluated
 
-env = PPO_trainer.run(experiment_name)
+env = PPO_trainer.run(experiment_name, experiment_description, manual_control=manual_control, only_test=only_test)
 PPO_testing.run(experiment_name, env, deterministic)
