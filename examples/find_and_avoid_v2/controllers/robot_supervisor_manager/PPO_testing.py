cacf0c3ce1e398a235f241fd470f8dc7ae00dbbc
tsampazk <27914645+tsampazk@users.noreply.github.com>
2023-03-20 15:58:36 +0200

    Cleanup, refactoring and improvements

--- examples/find_and_avoid_v2/controllers/robot_supervisor_manager/PPO_testing.py
+++ examples/find_and_avoid_v2/controllers/robot_supervisor_manager/PPO_testing.py
@@ -9,9 +9,20 @@ def mask_fn(env):
     return env.get_action_mask()
 
 
-def run(experiment_name, env, deterministic):
+def run(experiment_name, env, deterministic, use_masking, testing_results_filename=None, tests_per_difficulty=100,
+        seed=None):
     # Difficulty setup
-    difficulty_dict = {"diff_1": {"type": "corridor", "number_of_obstacles": 2,
+    """
+    - diff_0: Corridor without obstacles, varied distance, trivial
+    - diff_1: Corridor map with one row of two obstacles. Forces the agent to move around obstacles.
+    - diff_2: Corridor map with two rows of four obstacles. Same as before but more difficult.
+    - diff_3: Corridor map with three rows of six obstacles. Same as before but more difficult.
+    - diff_4: Corridor map with four of eight obstacles. Same as before but more difficult.
+    - diff_5: Random map with all available obstacles. Final complex difficulty in a more general environment.
+    """
+    difficulty_dict = {"diff_0": {"type": "corridor", "number_of_obstacles": 0,
+                                  "min_target_dist": 1, "max_target_dist": 3},
+                       "diff_1": {"type": "corridor", "number_of_obstacles": 2,
                                   "min_target_dist": 2, "max_target_dist": 2},
                        "diff_2": {"type": "corridor", "number_of_obstacles": 4,
                                   "min_target_dist": 3, "max_target_dist": 3},
@@ -19,30 +30,37 @@ def run(experiment_name, env, deterministic):
                                   "min_target_dist": 4, "max_target_dist": 4},
                        "diff_4": {"type": "corridor", "number_of_obstacles": 8,
                                   "min_target_dist": 5, "max_target_dist": 5},
-                       "random_diff": {"type": "random", "number_of_obstacles": 25,
-                                       "min_target_dist": 5, "max_target_dist": 12}}
-    test_difficulty = list(difficulty_dict.keys())
+                       "diff_5": {"type": "random", "number_of_obstacles": 25,
+                                  "min_target_dist": 5, "max_target_dist": 12}}
+    difficulty_keys = list(difficulty_dict.keys())
 
-    seed = 2
-    env.set_maximum_episode_steps(env.maximum_episode_steps * 2)
-    # Set default reward weights to make rewards comparable
-    env.set_reward_weight_dict(2, 2, 1, 1000, 1000, 10, 0.1, 0.0)
+    env.reset_on_collisions = -1  # Never terminate on collisions
+    env.set_maximum_episode_steps(env.maximum_episode_steps * 2)  # Double episode timeout limit
+    # Set default reward weights to make rewards comparable on testing
+    env.set_reward_weight_dict(target_distance_weight=0.0, target_angle_weight=0.0, dist_sensors_weight=0.0,
+                               target_reach_weight=1000.0, collision_weight=1.0,
+                               smoothness_weight=0.0, speed_weight=0.0)
     experiment_dir = f"./experiments/{experiment_name}"
-    load_path = experiment_dir + f"/{experiment_name}_diff_5_agent.zip"
+    load_path = experiment_dir + f"/{experiment_name}_diff_5_agent"
 
     if seed is not None:
         random.seed(seed)
         torch.manual_seed(seed)
         np.random.seed(seed)
 
-    model = MaskablePPO.load(load_path)  # NOQA
+    try:
+        model = MaskablePPO.load(load_path)  # NOQA
+    except FileNotFoundError:
+        load_path += ".zip"
+        model = MaskablePPO.load(load_path)  # NOQA
 
-    env.set_difficulty(difficulty_dict["random_diff"], "random_diff")
+    env.set_difficulty(difficulty_dict["diff_5"], "diff_5")
     print("################### SB3 EVALUATION STARTED ###################")
     print("Evaluating for 100 episodes in diff_5 (random map).")
     print(f"Experiment name: {experiment_name}, deterministic: {deterministic}")
     # Evaluate the agent with sb3
-    mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=100, deterministic=False)
+    mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=tests_per_difficulty,
+                                              deterministic=deterministic, use_masking=use_masking)
     print("################### SB3 EVALUATION FINISHED ###################")
     print(f"Experiment name: {experiment_name}, deterministic: {deterministic}")
     print(f"Mean reward: {mean_reward} \n"
@@ -51,20 +69,21 @@ def run(experiment_name, env, deterministic):
     print("################### CUSTOM EVALUATION STARTED ###################")
     print(f"Experiment name: {experiment_name}, deterministic: {deterministic}")
     diff_ind = 0
-    env.set_difficulty(difficulty_dict[test_difficulty[diff_ind]], test_difficulty[diff_ind])
-
-    tests_per_difficulty = 100
+    env.set_difficulty(difficulty_dict[difficulty_keys[diff_ind]], difficulty_keys[diff_ind])
 
     import csv
     header = [experiment_name]
-    for i in range(len(test_difficulty)):
+    for i in range(len(difficulty_keys)):
         for j in range(tests_per_difficulty):
-            header.append(f"{test_difficulty[i]}")
+            header.append(f"{difficulty_keys[i]}")
 
     episode_rewards = ["reward"]
     done_reasons = ["done_reason"]
     steps_row = ["steps"]
-    file_name = "/testing_results.csv" if not deterministic else "/testing_results_det.csv"
+    if testing_results_filename is None:
+        file_name = "/testing_results.csv" if not deterministic else "/testing_results_det.csv"
+    else:
+        file_name = f"/{testing_results_filename}.csv" if not deterministic else f"/{testing_results_filename}_det.csv"
     with open(experiment_dir + file_name, 'w', encoding='UTF8') as f:
         writer = csv.writer(f)
         writer.writerow(header)
@@ -74,7 +93,10 @@ def run(experiment_name, env, deterministic):
         obs = env.reset()
         successes_counter = 0
         while True:
-            action_masks = mask_fn(env)
+            if use_masking:
+                action_masks = mask_fn(env)
+            else:
+                action_masks = None
             action, _states = model.predict(obs, deterministic=deterministic, action_masks=action_masks)
             obs, rewards, done, info = env.step(action)
             steps += 1
@@ -84,9 +106,9 @@ def run(experiment_name, env, deterministic):
                 steps_row.append(steps)
                 if info["done_reason"] == "reached target":
                     successes_counter += 1
-                print(f"Episode reward: {cumulative_rew}, steps: {steps}")
-                full_test_count = (tests_per_difficulty * diff_ind) + tests_count
-                max_test_count = tests_per_difficulty * len(test_difficulty)
+                print(f"{experiment_name} - Episode reward: {cumulative_rew}, steps: {steps}")
+                full_test_count = (tests_per_difficulty * diff_ind) + (tests_count + 1)
+                max_test_count = tests_per_difficulty * len(difficulty_keys)
                 try:
                     print(f"Reached target percentage: {100 * successes_counter / full_test_count} %")
                 except ZeroDivisionError:
@@ -102,7 +124,7 @@ def run(experiment_name, env, deterministic):
                 if tests_count == tests_per_difficulty:
                     diff_ind += 1
                     try:
-                        env.set_difficulty(difficulty_dict[test_difficulty[diff_ind]], key=test_difficulty[diff_ind])
+                        env.set_difficulty(difficulty_dict[difficulty_keys[diff_ind]], key=difficulty_keys[diff_ind])
                     except IndexError:
                         print("Testing complete.")
                         break
@@ -125,7 +147,7 @@ def run(experiment_name, env, deterministic):
         cumulative_rew += rewards
         steps += 1
         if done:
-            print(f"Episode reward: {cumulative_rew}, steps: {steps}")
+            print(f"{experiment_name} - Episode reward: {cumulative_rew}, steps: {steps}")
             cumulative_rew = 0.0
             steps = 0
             obs = env.reset()
